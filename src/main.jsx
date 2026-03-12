1426518f7124cd571cceb439cddae649f9143aac
Samwise <marcintreder+samwise@gmail.com>
2026-03-11 20:02:42 -0700

    fix: updated identity to samwisegamgee@agentmail.to

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 // Triggering fresh Vercel build
 // Vercel Auth Sync
 // Vercel Trigger Wed Mar 11 19:40:34 PDT 2026
+// AgentMail Identity Update
