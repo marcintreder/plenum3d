a16eaa9c78af5eb68b8b312f762c55aa0fb888dc
Samwise Gamgee <samwisegamgee@agentmail.to>
2026-03-13 07:54:09 -0700

    Revert "optimize: reduce cylinder segments for mobile"
    This reverts commit 81729ccc02d8999457a56924fb0147852daaa3c5.

--- src/f1Model.jsx
+++ src/f1Model.jsx
@@ -19,7 +19,7 @@ export const generateF1 = () => {
         0,2,1, 0,3,2, 4,5,6, 4,6,7, 0,1,5, 0,5,4, 2,3,7, 2,7,6, 0,4,7, 0,7,3, 1,2,6, 1,6,5
       ];
     } else if (type === 'cylinder') {
-      const segments = 16; // Reduced segments for mobile device optimization
+      const segments = 32;
       const radius = w;
       const length = h;
       
